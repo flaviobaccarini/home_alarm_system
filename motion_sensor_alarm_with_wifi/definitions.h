61b7994a12e17bd72eeac35c805b5306b8390338
Flavio Baccarini <74458312+flaviobaccarini@users.noreply.github.com>
2023-01-16 17:33:54 +0100

    Update definitions.h

--- motion_sensor_alarm_with_wifi/definitions.h
+++ motion_sensor_alarm_with_wifi/definitions.h
@@ -5,21 +5,21 @@
 #define redLed 2       //  pin for red led
 #define greenLed 4        // pin for green led
 #define yellowLed 3    // pin for blue led
-#define buzzer A6
-#define blueLed_for_wifi A7
-#define c4 12
-#define c3 11
-#define c2 10
-#define c1 9
-#define r4 8
-#define r3 7
-#define r2 6
-#define r1 5
-#define rs A0
-#define en A1
-#define d4 A2
-#define d3 A3
-#define d2 A4
-#define d1 A5
+#define buzzer A6      //pin for buzzer
+#define blueLed_for_wifi A7  //pin for wifi led (blue or white)
+#define c4 12         //column 4 keypad
+#define c3 11         //column 3 keypad
+#define c2 10         //column 2 keypad
+#define c1 9          //column 1 keypad
+#define r4 8          //row 4 keypad
+#define r3 7          //row 3 keypad
+#define r2 6          //row 2 keypad
+#define r1 5          //row 1 keypad
+#define rs A0         //rs pin for lcd
+#define en A1         //en pin for lcd
+#define d4 A2         //d4 pin for lcd
+#define d3 A3         //d3 pin for lcd
+#define d2 A4         //d2 pin for lcd
+#define d1 A5         //d1 pin for lcd
 
 #endif
