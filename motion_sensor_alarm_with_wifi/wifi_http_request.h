5d7ad5c2bca0866aa2d74264c5a990625015411c
Flavio Baccarini <74458312+flaviobaccarini@users.noreply.github.com>
2023-01-16 17:31:49 +0100

    Update wifi_http_request.h

--- motion_sensor_alarm_with_wifi/wifi_http_request.h
+++ motion_sensor_alarm_with_wifi/wifi_http_request.h
@@ -10,12 +10,11 @@ class Wifi_http_request{
     char*   HOST_NAME_;
     String PATH_NAME_;
     String queryString_;
-    // variables will change:
-    int blueled_for_Wifi_;
+    int blueled_for_Wifi_; //led for the wifi: if wifi is on the led is on, if wifi is off the led is of
     char* ssid;
     char* pass;
-    int delay_time_for_reconnection = 20000;
-    unsigned long time_stop, time_connection;
+    int delay_time_for_reconnection = 20000; //delay time between the stop of the client and the next reconnection (20 seconds)
+    unsigned long time_stop, time_connection; //variables for the reconnection, avoiding the delay function
 
   public:
     bool client_connection = 0; // if the server is connected or not
